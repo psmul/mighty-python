1ae39fb865d394499a2eb5947e66149b7b10f41b
Patryk Smul <patryksmul@outlook.com>
2019-01-17 20:29:14 +0100

    clean up

--- vsearch/vsearch.py
+++
@@ -1,3 +0,0 @@
-def search4letters(phrase: str, letters: str = 'te') -> set:
-    """ Return any letters defined in second parameter from phrase """
-    return set(letters).intersection(set(phrase))
